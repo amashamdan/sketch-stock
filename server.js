c833c358d07b6b7f56fa5d738817f5af75c383e0
Amer Hamdan <amashamdan@gmail.com>
2016-11-17 10:43:23 -0800

    Feat: The server now fetches the stock data for companies found in the database.
    Added get request to /load-data route.
    This looks up the conpaines' symbols from the database then requests stock information from yahoo-finance api.
    Once loaded, the results are sent as response to the client.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require("express");
 //var secure = require("express-force-https");
 var mongodb = require("mongodb");
 var ejs = require("ejs");
+var yahooFinance = require("yahoo-finance");
 
 var app = express();
 //app.use(secure);
 
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
+app.use("/scripts", express.static(__dirname + "/views/scripts"));
 
 var MongoClient = mongodb.MongoClient;
 var mongoUrl = process.env.STOCKS;
@@ -15,8 +17,24 @@ MongoClient.connect(mongoUrl, function(err, db) {
 	if (err) {
 		res.end("Failed to connect to database.")
 	} else {
+		var companies = db.collection("companies");
 		app.get("/", function(req, res) {
 			res.render("index.ejs");
+		});
+
+		app.get("/load-data", function(req, res) {
+			var symbols = [];
+			companies.find({}).toArray(function(err, result) {
+				symbols = result[0].symbols;
+				yahooFinance.historical({
+					symbols: symbols,
+					from: "2015-01-01",
+					to: "2016-01-01"
+				}, function (err, result) {
+					res.send(result);
+					res.end();
+				});
+			});
 		});
 	}
 });
