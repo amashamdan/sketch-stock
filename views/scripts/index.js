d3964a4da4163d8fee22335d8cad8c0a922a000c
Amer Hamdan <amashamdan@gmail.com>
2016-11-18 11:56:38 -0800

    Feat: 'Currently showing' section now shows the displayed stocks.
    Each company has its own rectangle with its name and delete button.
    Delete button hasn't been activated yet.

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -24,13 +24,10 @@ $(document).ready(function() {
 	    var seriesOptions = [];
 
 	    function createChart() {
-
 	        Highcharts.stockChart('chart', {
-
 	            rangeSelector: {
 	                selected: 4
 	            },
-
 	            yAxis: {
 	                labels: {
 	                    formatter: function () {
@@ -43,38 +40,30 @@ $(document).ready(function() {
 	                    color: 'red'
 	                }]
 	            },
-
 	            colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-
 	            plotOptions: {
 	                series: {
 	                    compare: 'percent',
 	                    showInNavigator: true
 	                }
 	            },
-
 	            tooltip: {
 	                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
 	                valueDecimals: 2,
 	                split: false
 	            },
-
 	            series: seriesOptions
 	        });
 	    }
 
 	    $.each(names, function (i, name) {
-
-	            seriesOptions[i] = {
-	                name: name,
-	                data: data[i]
-	            };
-	                
-	            
-	        });
-	    createChart();
+            seriesOptions[i] = {
+                name: name,
+                data: data[i]
+            };
+            $(".companies").append("<div class='company-div'><h4 class='company-header'>"+ name +"</h4><button class='delete-button'>X</button></div>");           
 	    });
-
-
-
+	    
+	    createChart();
 	});
+});
