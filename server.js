8056f42de35d97d78a76205a644a1e1ad6c7c041
Amer Hamdan <amashamdan@gmail.com>
2016-11-18 19:10:39 -0800

    Refactor and Feat: User can remove companies from database.
    Refactor: Created new function getStock which call yahoo-finance API to get stock data.
    This is done becuase stock data are not stored in the database.
    When the user adds or deletes companies, the API will be called again, so a function is created for that to avoid code dublications.
    Feat: When the user clicks the delete button, the company gets removed from the database.

--- server.js
+++ server.js
@@ -23,28 +23,41 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		});
 
 		app.get("/load-data", function(req, res) {
-			var time = new Date();
-			var currentYear = time.getYear() + 1900;
-			var pastYear = currentYear - 1;
-			var day = time.getDate();
-			var month = time.getMonth() + 1;
+			getStock(res,companies);
+		});
 
-			var symbols = [];
-			companies.find({}).toArray(function(err, result) {
-				symbols = result[0].symbols;
-				yahooFinance.historical({
-					symbols: symbols,
-					from: pastYear + "-" + month + "-" + day,
-					to: currentYear + "-" + month + "-" + day
-				}, function (err, result) {
-					res.send(result);
-					res.end();
-				});
-			});
+		app.delete("/delete/:name", function(req, res) {
+			companies.update(
+				{},
+				{"$pull": {"symbols": req.params.name}},
+				function() {
+					getStock(res, companies);
+				}
+			);
 		});
 	}
 });
 
+function getStock(res, companies) {
+	var time = new Date();
+	var currentYear = time.getYear() + 1900;
+	var pastYear = currentYear - 1;
+	var day = time.getDate();
+	var month = time.getMonth() + 1;
+
+	var symbols = [];
+	companies.find({}).toArray(function(err, result) {
+		symbols = result[0].symbols;
+		yahooFinance.historical({
+			symbols: symbols,
+			from: pastYear + "-" + month + "-" + day,
+			to: currentYear + "-" + month + "-" + day
+		}, function (err, result) {
+			res.send(result);
+			res.end();
+		});
+	});
+}
 
 var port = Number(process.env.PORT || 443);
 app.listen(port);
