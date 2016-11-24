d0d6565de4e1b7d3101365d50b3d5d7dba771485
Amer Hamdan <amashamdan@gmail.com>
2016-11-23 18:19:53 -0800

    Bug: Changed address in io.connect method to https://sketch-stock.herokuapp.com/
    It was set to localhost 8080 previously, and the page wouldn't work once deployed on heroku.

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:8080");
+var socket = io.connect("https://sketch-stock.herokuapp.com/");
 $(document).ready(function() {
 	socket.on("results", function(results) {
 		$(".company-div").remove();
