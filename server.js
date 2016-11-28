562e5e6f77a426482c45a52184f0c21ac0309ce4
Amer <amashamdan@gmail.com>
2016-11-28 14:41:16 -0800

    Refactor: Call to yahoo-finance api removed.
    The client now calls yahoo-finance api.
    Server only adds and removes data from the database.

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var server = require('http').createServer(app);
 var io = require("socket.io")(server);
 
 var ejs = require("ejs");
-var yahooFinance = require("yahoo-finance");
 
 
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
@@ -51,25 +50,13 @@ MongoClient.connect(mongoUrl, function(err, db) {
 });
 
 function getStock(companies, client, isCompaniesModified) {
-	var time = new Date();
-	var currentYear = time.getYear() + 1900;
-	var pastYear = currentYear - 1;
-	var day = time.getDate();
-	var month = time.getMonth() + 1;
-
 	var symbols = [];
 	companies.find({}).toArray(function(err, result) {
 		symbols = result[0].symbols;
-		yahooFinance.historical({
-			symbols: symbols,
-			from: pastYear + "-" + month + "-" + day,
-			to: currentYear + "-" + month + "-" + day
-		}, function (err, result) {
-			if (isCompaniesModified) {
-				client.broadcast.emit("results", result);
-			}
-			client.emit("results", result);
-		});
+		if (isCompaniesModified) {
+			client.broadcast.emit("results", result);
+		}
+		client.emit("results", symbols);
 	});
 }
 
